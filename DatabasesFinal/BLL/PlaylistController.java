6155a83f76dd1a854430199c5cc2c77b5c5d804c
JSmith1140 <168117141+JSmith1140@users.noreply.github.com>
2026-05-07 09:39:36 -0400

    added comments to PlaylistController.java

--- DatabasesFinal/BLL/PlaylistController.java
+++ DatabasesFinal/BLL/PlaylistController.java
@@ -9,23 +9,53 @@ public class PlaylistController {
 
     private PlaylistDataProvider playlistDP = new PlaylistDataProvider();
 
-    // User input goes from controller to DataProvider to be inserted into DB
+    /***
+     * create playlist
+     * @param userId
+     * @param playlistName
+     * @return
+     * @throws Exception
+     */
     public int createPlaylist(int userId, String playlistName) throws Exception {
         return playlistDP.createPlaylist(userId, playlistName);
     }
 
+    /***
+     * add song to playlist
+     * @param playlistId
+     * @param songId
+     * @throws Exception
+     */
     public void addSongToPlaylist(int playlistId, int songId) throws Exception {
         playlistDP.addSongToPlaylist(playlistId, songId);
     }
 
+    /***
+     * blacklist song
+     * @param userId
+     * @param songId
+     * @throws Exception
+     */
     public void blacklistSong(int userId, int songId) throws Exception {
         playlistDP.blacklistSong(userId, songId);
     }
 
+    /***
+     * genre stats
+     * @param userId
+     * @throws Exception
+     */
     public void genreStats(int userId) throws Exception {
         GenreStatsDataProvider.getGenreStats(userId);
     }
 
+    /***
+     * playlist stats
+     * @param userId
+     * @param playlistName
+     * @return
+     * @throws Exception
+     */
     public List<PlaylistStat> playlistStats(int userId, String playlistName) throws Exception {
         return playlistDP.getPlaylistStats(userId, playlistName);
     }
