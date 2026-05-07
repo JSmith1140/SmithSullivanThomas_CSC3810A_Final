a108e6399d481c79545b4f7224b737439c7682b8
JSmith1140 <168117141+JSmith1140@users.noreply.github.com>
2026-05-07 09:41:37 -0400

    added comments to ScoredSong.java

--- DatabasesFinal/BLL/ScoredSong.java
+++ DatabasesFinal/BLL/ScoredSong.java
@@ -4,11 +4,14 @@ public class ScoredSong {
     private SongCandidate song;
     private int score;
 
+    /***
+     * scored song constructor
+     */
     public ScoredSong(SongCandidate song, int score) {
         this.song = song;
         this.score = score;
     }
 
-    public SongCandidate getSong() { return song; }
-    public int getScore() { return score; }
+    public SongCandidate getSong() { return song; } // get song
+    public int getScore() { return score; } // get score
 }
